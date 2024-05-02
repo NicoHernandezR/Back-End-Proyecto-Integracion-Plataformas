36c28bab08f8b054875eaa4444ccd391e097dabb
Makenion <es.tamayo@duocuc.cl>
2024-05-01 22:12:51 -0400

    Prueba
    Prueba

---
+++ src/main/kotlin/api/ferremas/ferrmas/tipoProducto/TipoProductoModel.kt
@@ -0,0 +1,10 @@
+package api.ferremas.ferrmas.tipoProducto
+
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tipoProducto")
+class TipoProductoModel (
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) var id: Long? = null,
+    var tipo: String? = null,){
+}
